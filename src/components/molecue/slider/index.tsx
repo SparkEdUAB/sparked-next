3b2478b66b31a9759536109e5fff49e26ef6804a
Brian Mukuka <brianmuks@gmail.com>
2024-02-23 17:47:08 +0200

    home page slider

---
+++ src/components/molecue/slider/index.tsx
@@ -0,0 +1,185 @@
+import { Carousel } from "flowbite-react";
+import styled from "styled-components";
+
+
+
+const sliderTextBkgUrl = "assets/images/cover/slider-text-bkg.png";
+// const sliderTextBkgUrl =
+//   "https://img.freepik.com/free-vector/hand-drawn-back-school-pattern-collection_23-2149037428.jpg?t=st=1708695518~exp=1708699118~hmac=53adb27916bfc355d4bf998ac088740556fa7e0818dc3a2c78300234b2bf164b&w=1060";
+
+type T_imageContainer = {
+  imageUrl: string;
+};
+
+type T_sliderTextContainer = {
+  sliderTextBkgUrl: string;
+};
+
+const ImageContainer = styled.div<T_imageContainer>`
+  width: 100%;
+  /* height: 100px; */
+  background-image: url(${(p) => p.imageUrl});
+
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-color: #cbcacc !important;
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: row;
+  filter: grayscale(100%);
+`;
+
+const SliderImage = styled.img`
+  object-fit: cover;
+  /* width: ; */
+  /* height: 100px; */
+`;
+const SliderTextContainer = styled.div<T_sliderTextContainer>`
+  width: 52%;
+  height: 100%;
+  border-radius: 2%;
+  /* border: 3px dotted #049ba0; */
+  display: flex;
+  /* justify-content: center; */
+  /* align-items: center; */
+
+  /* margin-bottom: auto; */
+  background-image: url(${(p) => p.sliderTextBkgUrl});
+  background-size: cover;
+  background-repeat: no-repeat;
+
+  -webkit-filter: blur(5px);
+  -moz-filter: blur(5px);
+  -o-filter: blur(5px);
+  -ms-filter: blur(5px);
+  filter: blur(5px);
+`;
+
+
+const SliderTitle = styled.p`
+  text-align: center;
+  right: 0;
+  top:0;
+  margin:5px;
+  width: 50%;
+  height: 100px;
+  position: absolute;
+  font-size: 48px;
+  color: white;
+  font-weight: 700;
+`;
+
+const SliderText = styled.p`
+  text-align: center;
+  right: 0;
+  top: 60%;
+  margin: 5px;
+  width: 50%;
+  height: 100px;
+  position: absolute;
+  font-size: 24px;
+  color: white;
+  font-weight: 700;
+`;
+
+const SliderCarousel = styled(Carousel)`
+  height: 400px;
+`;
+
+export const HomePageSlider = () => {
+  return (
+    <div className="h-56 sm:h-64 xl:h-80 2xl:h-96">
+      <SliderCarousel pauseOnHover>
+        <ImageContainer
+          imageUrl="https://img.freepik.com/free-photo/group-international-students-sitting-grass-together-park-university-african-caucasian-girls-indian-boy-talking-outdoors_1157-50065.jpg?t=st=1708684780~exp=1708688380~hmac=3bd13570c8efd61351953e248e06c6faf95d896f2eb3cdae3b487bea01804017&w=1800"
+          className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white"
+        >
+          <SliderTextContainer
+            sliderTextBkgUrl={sliderTextBkgUrl}
+          ></SliderTextContainer>
+          <SliderTitle>
+            Welcome to Sparked - your gateway to knowledge!
+          </SliderTitle>
+
+          <SliderText>
+            Explore a world of learning with our extensive collection of books,
+            magazines, and resources. Let's ignite your curiosity and spark your
+            imagination
+          </SliderText>
+        </ImageContainer>
+
+        <ImageContainer
+          imageUrl="https://img.freepik.com/free-photo/side-view-woman-working-as-travel-agent_23-2150433458.jpg?t=st=1708702733~exp=1708706333~hmac=e4a1690efa66d45683bf2f58364e65128efa548abe3bafac774427f0993b1635&w=1800"
+          className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white"
+        >
+          <SliderTextContainer
+            sliderTextBkgUrl={sliderTextBkgUrl}
+          ></SliderTextContainer>
+          <SliderTitle>
+            Presenting Educational And Training Content With Passion And AI.{" "}
+          </SliderTitle>
+
+          <SliderText>
+            Discover a revolutionary approach to educational and training
+            content delivery. At the intersection of passion and artificial
+            intelligence (AI), our platform redefines the learning experience
+          </SliderText>
+        </ImageContainer>
+
+        <ImageContainer
+          className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white"
+          imageUrl="https://img.freepik.com/free-photo/cheerful-man-classroom-posing-camera_23-2147663989.jpg?t=st=1708685035~exp=1708688635~hmac=8eb9796885031de81ed4c50e28467fed995bf0440ec6fdf08b49e566d727ee7d&w=1800"
+        >
+          <SliderTextContainer
+            sliderTextBkgUrl={sliderTextBkgUrl}
+          ></SliderTextContainer>
+          <SliderTitle>
+            Discover what's new at Sparked! Explore our latest arrivals
+          </SliderTitle>
+
+          <SliderText>
+            from captivating novels to informative guides. Stay ahead of the
+            curve and dive into fresh reads to fuel your curiosity."
+          </SliderText>
+        </ImageContainer>
+
+        <ImageContainer
+          imageUrl="https://img.freepik.com/free-photo/group-four-african-american-girls-walking-stairs-city_627829-2812.jpg?t=st=1708684880~exp=1708688480~hmac=2c669e521f8474c34d8bf28f4a3459309a157203b6e8093eb5c6bb8e9dd1b4d4&w=1800"
+          className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white"
+        >
+          <SliderTextContainer
+            sliderTextBkgUrl={sliderTextBkgUrl}
+          ></SliderTextContainer>
+          <SliderTitle>
+            Check out Sparked's featured books! Immerse yourself in captivating
+            stories
+          </SliderTitle>
+
+          <SliderText>
+            insightful non-fiction, and educational resources handpicked by our
+            team. Find your next favorite read and let your imagination soar!"
+          </SliderText>
+        </ImageContainer>
+
+        <ImageContainer
+          imageUrl="https://img.freepik.com/free-photo/teacher-helping-kids-class_23-2148892533.jpg?t=st=1708702783~exp=1708706383~hmac=77580a907ee62f0b379966e992eb285eb10eea0b382d87f72b78b0ef7c8cdfd6&w=1800"
+          className="flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white"
+        >
+          <SliderTextContainer
+            sliderTextBkgUrl={sliderTextBkgUrl}
+          ></SliderTextContainer>
+          <SliderTitle>
+            Join us for exciting events at Sparked! Be on the lookout for
+            upcoming author visits
+          </SliderTitle>
+
+          <SliderText>
+            Don't miss out on book clubs, reading challenges, and more. Get
+            involved in our vibrant community and let's spark your love for
+            reading together!"
+          </SliderText>
+        </ImageContainer>
+      </SliderCarousel>
+    </div>
+  );
+};
--- src/components/welcomePage/index.tsx
+++ src/components/welcomePage/index.tsx
@@ -1,62 +1,73 @@
 "use client";
 
 import React from "react";
-import { Button, Card } from "flowbite-react";
+import { Button, Card,Carousel } from "flowbite-react";
 import GuestLayout from "../layouts/guestLayout";
 import { CourseBasedSvgImage, EducationSvgImage, UniversitySvgImage } from "@components/svgs";
+import styled from "styled-components";
+import { HomePageSlider } from "@components/molecue/slider";
+
+
+const StyledMainContentCard = styled(Card)`
+height: 250px;
+/* background-color: red; */
+`
+
+const StyledSubContentCard = styled(Card)`
+  max-height: 300px;
+`;
 
 const  WelcomePage:React.FC = ()=> {
 
   return (
     <GuestLayout>
       <div className="landing-page-container">
-        <h5 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-white text-center">
-          <Card>
-            <p>
-              Presenting Educational And Training Content With Passion And AI.{" "}
-            </p>
-          </Card>
-        </h5>
-
-        <div className="relative h-40 w-10 bg-blue-300 rounded-lg shadow-lg overflow-hidden">
-          <div className="absolute top-0 right-0 bg-blue-500 h-8 w-8 transform rotate-45 origin-top-left"></div>
-        </div>
+        <HomePageSlider />
 
         <div className="flex justify-between landing-page-main-card-container">
-          <Card className="max-w-sm bg-blue-200  " href="#">
+          <StyledMainContentCard className="max-w-sm bg-blue-200  " href="#">
             <CourseBasedSvgImage />
-            <Card className="max-w-sm " href="#">
+            <StyledSubContentCard className="max-w-sm" href="#">
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 <p>Course Based</p>
               </h5>
               <p className="font-normal text-gray-700 dark:text-gray-400">
                 <p>Organize Contents for Colleges with Different Courses</p>
               </p>
-            </Card>
-          </Card>
+              <Button>
+                <p>Read more</p>
+              </Button>
+            </StyledSubContentCard>
+          </StyledMainContentCard>
 
-          <Card
+          <StyledMainContentCard
             className="max-w-sm landing-page-plain-card bg-orange-100"
             href="#"
           >
             <UniversitySvgImage />
-            <div className="project-card bg-blue-200"></div>
-            <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-              <p className="text-center">University Based</p>
-            </h5>
-            <p className="font-normal text-gray-700 dark:text-gray-400">
-              <p>
-                Ability to organize contents following the University structure
-              </p>
-            </p>
-            <Button>
-              <p>Read more</p>
-            </Button>
-          </Card>
 
-          <Card horizontal>
+            <StyledSubContentCard className="max-w-sm" href="#">
+              <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                <p className="text-center">University Based</p>
+              </h5>
+              <p className="font-normal text-gray-700 dark:text-gray-400">
+                <p>
+                  Ability to organize contents following the University
+                  structure
+                </p>
+              </p>
+              <Button>
+                <p>Read more</p>
+              </Button>
+            </StyledSubContentCard>
+          </StyledMainContentCard>
+
+          <StyledMainContentCard
+            className="max-w-sm bg-green-400 "
+            href="#"
+          >
             <EducationSvgImage />
-            <Card className="max-w-sm bg-green-400 " href="#">
+            <StyledSubContentCard>
               <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
                 <p>High School</p>
               </h5>
@@ -66,8 +77,11 @@ const  WelcomePage:React.FC = ()=> {
                   schools manage their contents
                 </p>
               </p>
-            </Card>
-          </Card>
+              <Button>
+                <p>Read more</p>
+              </Button>
+            </StyledSubContentCard>
+          </StyledMainContentCard>
         </div>
       </div>
     </GuestLayout>
