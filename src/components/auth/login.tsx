9409b1bbb4ab18a83e20008a1d52629a65bce275
Olivier JM Maniraho <manolivier93@gmail.com>
2024-02-25 09:06:27 +0200

    remove unused files

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,73 +1,86 @@
-import { Col, Form, Input, Row } from "antd";
+import {  Form, Input } from "antd";
 import i18next from "i18next";
-import { CourseBasedSvgImage } from "@components/svgs";
-import { Card } from "antd";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 import { Button } from "flowbite-react";
-// @todo use login form fields 
+// @todo use login form fields
 import { SIGNUP_FORM_FIELDS } from "./constants";
 import useAuth from "@hooks/useAuth";
+import Link from "next/link";
 
 const onFinishFailed = (errorInfo: any) => {};
 
 const Login: React.FC = () => {
-  const { handleLogin } = useAuth();
+  const { handleLogin, loading } = useAuth();
 
   return (
-    <Row className="auth-container">
-      <Col span={12}>
-        <CourseBasedSvgImage height={1000} width={1000} />
-      </Col>
-
-      <Col span={12}>
-        <Card
-          className="auth-card"
-          title={i18next.t("login")}
-          bordered={false}
-        >
-          <Form
-            labelCol={{ span: 8 }}
-            wrapperCol={{ span: 16 }}
-            style={{ maxWidth: 600 }}
-            initialValues={{ remember: true }}
-            onFinish={handleLogin}
-            onFinishFailed={onFinishFailed}
-            autoComplete="off"
+    <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+    }}
+  >
+    <Form
+      name="normal_login"
+      className="login-form"
+      onFinish={handleLogin}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        name={SIGNUP_FORM_FIELDS.email.key}
+        rules={[
+          {
+            required: true,
+            message: SIGNUP_FORM_FIELDS.email.errorMsg,
+          },
+        ]}
+        style={{ width: '400px' }}
+      >
+        <Input
+          size="large"
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder={SIGNUP_FORM_FIELDS.email.label}
+        />
+      </Form.Item>
+      <Form.Item
+        name={SIGNUP_FORM_FIELDS.password.key}
+        rules={[
+          {
+            required: true,
+            message: SIGNUP_FORM_FIELDS.password.errorMsg,
+          },
+        ]}
+        style={{ width: '400px' }}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder={SIGNUP_FORM_FIELDS.password.label}
+          size="large"
+        />
+      </Form.Item>
+      <Form.Item className="form-item-style">
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="button-style"
+            style={{
+              backgroundColor: '#1890ff',
+              borderColor: '#1890ff',
+              color: 'white',
+            }}
+            loading={loading}
           >
-            <Form.Item
-              label={SIGNUP_FORM_FIELDS.email.label}
-              name={SIGNUP_FORM_FIELDS.email.key}
-              rules={[
-                {
-                  required: true,
-                  message: SIGNUP_FORM_FIELDS.email.errorMsg,
-                },
-              ]}
-            >
-              <Input />
-            </Form.Item>
-
-            <Form.Item
-              label={SIGNUP_FORM_FIELDS.password.label}
-              name={SIGNUP_FORM_FIELDS.password.key}
-              rules={[
-                {
-                  required: true,
-                  message: SIGNUP_FORM_FIELDS.password.errorMsg,
-                },
-              ]}
-            >
-              <Input.Password />
-            </Form.Item>
-
-            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
-              <Button type="primary" htmlType="submit">
-                {i18next.t("submit")}
-              </Button>
-            </Form.Item>
-          </Form>
-        </Card>
-      </Col>
-    </Row>
+            {i18next.t('submit')}
+          </Button>
+          <span style={{ marginLeft: '10px' }}>Or</span>
+          <Link href="/auth/signup" style={{ marginLeft: '10px' }}>Register</Link>
+        </div>
+      </Form.Item>
+    </Form>
+  </div>
   );
 };
 
