34069024ea3b6febe7e0f3b81d8461084d1344b2
Olivier JM Maniraho <manolivier93@gmail.com>
2024-02-26 07:47:02 +0200

    some refactor

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {},
+  env: {
+    MONGODB_URI: "mongodb://example:testspassword@dbserver.example:5555/userdata?tls=true&connectionTimeout=5000"
+  },
   webpack: (config) => {
     config.externals = [...config.externals, { realm: "realm" }]; // required to make realm
     return config;
