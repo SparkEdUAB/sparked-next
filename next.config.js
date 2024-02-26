b18663eba26b8e79a29c28280d2610d8ae7c2d71
Olivier JM Maniraho <manolivier93@gmail.com>
2024-02-26 07:51:05 +0200

    Set default value for mongo uri

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
-    MONGODB_URI: "mongodb://example:testspassword@dbserver.example:5555/userdata?tls=true&connectionTimeout=5000"
+    MONGODB_URI: "mongodb://example:testspassword@dbserver.example:5555/userdata?tls=true"
   },
   webpack: (config) => {
     config.externals = [...config.externals, { realm: "realm" }]; // required to make realm
