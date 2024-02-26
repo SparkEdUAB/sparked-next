404e90dd47b8bffaf1b13726b5206cab8b5d0c09
Olivier JM Maniraho <manolivier93@gmail.com>
2024-02-26 08:03:15 +0200

    cache & run lint instead of build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  env: {
-    MONGODB_URI: "mongodb://example:testspassword@dbserver.example:5555/userdata?tls=true"
-  },
+  env: {},
   webpack: (config) => {
     config.externals = [...config.externals, { realm: "realm" }]; // required to make realm
     return config;
