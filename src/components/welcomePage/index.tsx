4f3c835b5379f4c464ddc79e78df9f8460631c67
Olivier JM Maniraho <manolivier93@gmail.com>
2024-02-27 19:05:28 +0200

    add some color

--- src/components/welcomePage/index.tsx
+++ src/components/welcomePage/index.tsx
@@ -5,61 +5,65 @@ import { Button, Flex } from 'antd';
 
 import Link from 'next/link';
 
+// TODO: divide this page in sections
+
 const WelcomePage: React.FC = () => {
   return (
     <>
       <header>
-        <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <nav className="bg-blue-50 border-blue-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
           <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://flowbite.com" className="flex items-center">
-              <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              <span className="self-center text-xl font-semibold text-blue-900 whitespace-nowrap dark:text-white">
                 SparkEd
               </span>
             </a>
             <div className="flex items-center lg:order-2">
               <Flex gap="small" wrap="wrap">
-                <Button type="link" >
+                <Button type="link" className="text-blue-600">
                   <Link href="/auth/login">Log in</Link>
                 </Button>
-                <Button type="text">Get started</Button>
+                <Button type="text" className="bg-blue-600 text-white">
+                  <Link href="/auth/signup">Get started</Link>
+                </Button>
               </Flex>
-
             </div>
           </div>
         </nav>
       </header>
 
-      <section className="bg-white dark:bg-gray-900">
+      <section className="bg-blue-50 dark:bg-gray-900">
         <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="place-self-center mr-auto lg:col-span-7">
-            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl dark:text-white">
-              Payments tool for software companies
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none text-blue-900 md:text-5xl xl:text-6xl dark:text-white">
+              Your digital library
             </h1>
-            <p className="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-              From checkout to global sales tax compliance, companies around the
-              world use Flowbite to simplify their payment stack.
+            <p className="mb-6 max-w-2xl font-light text-blue-700 lg:mb-8 md:text-lg lg:text-xl dark:text-blue-300">
+              Easily manage, your organization resources
             </p>
-            <Button size={"large"}>Get Started</Button>
+            <Button size={'large'} className="bg-blue-600 text-white">
+              Get Started
+            </Button>
           </div>
         </div>
       </section>
 
-      <section className="bg-gray-50 dark:bg-gray-800">
+      <section className="bg-blue-100 dark:bg-gray-800">
         <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
           <div className="mb-8 max-w-screen-md lg:mb-16">
-            <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
-              Designed for business teams like yours
+            <h2 className="mb-4 text-4xl font-extrabold text-blue-900 dark:text-white">
+              Designed for schools and large organizations
             </h2>
-            <p className="text-gray-500 sm:text-xl dark:text-gray-400">
-              Here at Flowbite we focus on markets where technology, innovation,
-              and capital can unlock long-term value and drive economic growth.
+            <p className="text-blue-700 sm:text-xl dark:text-blue-300">
+              SparkEd is designed for schools and organizations, SparkEd is
+              designed for schools and organizations.
             </p>
           </div>
           <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-            <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex flex-col items-center text-center">
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-200 lg:h-12 lg:w-12 dark:bg-blue-700">
                 <svg
-                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
@@ -71,113 +75,98 @@ const WelcomePage: React.FC = () => {
                   ></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
-                Marketing
+              <h3 className="mb-2 text-xl font-bold text-blue-900 dark:text-white">
+                Course based
               </h3>
-              <p className="text-gray-500 dark:text-gray-400">
-                Plan it, create it, launch it. Collaborate seamlessly with all
-                the organization and hit your marketing goals every month with
-                our marketing plan.
+              <p className="text-blue-700 dark:text-blue-300">
+                Organize contents for colleges with different courses
               </p>
             </div>
-            <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+            <div className="flex flex-col items-center text-center">
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-200 lg:h-12 lg:w-12 dark:bg-blue-700">
                 <svg
-                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path>
-                </svg>
-              </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">Legal</h3>
-              <p className="text-gray-500 dark:text-gray-400">
-                Protect your organization, devices and stay compliant with our
-                structured workflows and custom permissions made for you.
-              </p>
-            </div>
-            <div>
-              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                <svg
-                  className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
+                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
                   fill="currentColor"
                   viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg"
                 >
                   <path
                     fill-rule="evenodd"
-                    d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
+                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
                     clip-rule="evenodd"
                   ></path>
-                  <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path>
                 </svg>
               </div>
-              <h3 className="mb-2 text-xl font-bold dark:text-white">
-                Business Automation
+              <h3 className="mb-2 text-xl font-bold text-blue-900 dark:text-white">
+                University based
               </h3>
-              <p className="text-gray-500 dark:text-gray-400">
-                Auto-assign tasks, send Slack messages, and much more. Now power
-                up with hundreds of new templates to help you get started.
+              <p className="text-blue-700 dark:text-blue-300">
+                Ability to organize contents following the University structure
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-200 lg:h-12 lg:w-12 dark:bg-blue-700">
+                <svg
+                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold text-blue-900 dark:text-white">
+                High School based
+              </h3>
+              <p className="text-blue-700 dark:text-blue-300">
+                Contents can also be arranged as High Schools to help small
+                schools manage their contents
               </p>
             </div>
           </div>
         </div>
       </section>
 
-      <section className="bg-gray-50 dark:bg-gray-900 dark:bg-gray-800">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-          <div className="max-w-screen-lg text-gray-500 sm:text-lg dark:text-gray-400">
-            <h2 className="mb-4 text-4xl font-bold text-gray-900 dark:text-white">
-              Powering innovation at{' '}
-              <span className="font-extrabold">200,000+</span> companies
-              worldwide
+      <section className="bg-blue-50 dark:bg-gray-900">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6 flex justify-center">
+          <div className="max-w-screen-lg text-blue-700 sm:text-lg dark:text-blue-300">
+            <h2 className="mb-4 text-4xl font-bold text-blue-900 dark:text-white text-center">
+              Powering future school digital libraries
             </h2>
-            <p className="mb-4 font-light">
-              Track work across the enterprise through an open, collaborative
-              platform. Link issues across Jira and ingest data from other
-              software development tools, so your IT support and operations
-              teams have richer contextual information to rapidly respond to
-              requests, incidents, and changes.
+            <p className="mb-4 font-light text-center">
+              Manage and track educational content across your institution
+              through our open, collaborative platform. Link issues across
+              different departments and ingest data from various educational
+              tools, so your administrative and academic teams have richer
+              contextual information to rapidly respond to requests, changes,
+              and incidents. This digital approach ensures a seamless management
+              of school content.
             </p>
-            <p className="mb-4 font-medium">
-              Deliver great service experiences fast - without the complexity of
-              traditional ITSM solutions.Accelerate critical development work,
-              eliminate toil, and deploy changes with ease.
-            </p>
+
           </div>
         </div>
       </section>
 
-      <section className="bg-white dark:bg-gray-900">
-        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-          <div className="mx-auto max-w-screen-sm text-center">
-            <h2 className="mb-4 text-4xl font-extrabold leading-tight text-gray-900 dark:text-white">
-              Give it a try
-            </h2>
-            <p className="mb-6 font-light text-gray-500 dark:text-gray-400 md:text-lg">
-              Try SparkEd Platform and easily manage your resources
-            </p>
-          </div>
-        </div>
-      </section>
-
-      <footer className="p-4 bg-gray-50 sm:p-6 dark:bg-gray-800">
+      <footer className="p-4 bg-blue-50 sm:p-6 dark:bg-gray-800">
         <div className="mx-auto max-w-screen-xl">
           <div className="md:flex md:justify-between">
             <div className="mb-6 md:mb-0">
               <a href="https://flowbite.com" className="flex items-center">
-                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                <span className="self-center text-2xl font-semibold text-blue-900 whitespace-nowrap dark:text-white">
                   SparkEd
                 </span>
               </a>
             </div>
             <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
               <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <h2 className="mb-6 text-sm font-semibold text-blue-900 uppercase dark:text-white">
                   Follow us
                 </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
+                <ul className="text-blue-700 dark:text-blue-300">
                   <li className="mb-4">
                     <a
                       href="https://github.com/sparkeduab/sparked-next"
@@ -189,10 +178,10 @@ const WelcomePage: React.FC = () => {
                 </ul>
               </div>
               <div>
-                <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+                <h2 className="mb-6 text-sm font-semibold text-blue-900 uppercase dark:text-white">
                   Legal
                 </h2>
-                <ul className="text-gray-600 dark:text-gray-400">
+                <ul className="text-blue-700 dark:text-blue-300">
                   <li className="mb-4">
                     <a href="#" className="hover:underline">
                       Privacy Policy
@@ -202,9 +191,9 @@ const WelcomePage: React.FC = () => {
               </div>
             </div>
           </div>
-          <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+          <hr className="my-6 border-blue-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
           <div className="sm:flex sm:items-center sm:justify-between">
-            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+            <span className="text-sm text-blue-700 sm:text-center dark:text-blue-300">
               © 2024{' '}
               <a href="" className="hover:underline">
                 SparkEd™
