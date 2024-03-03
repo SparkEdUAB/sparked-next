80e79d4d844ac16c4ad86d572759921d2a4bd5c5
Olivier JM Maniraho <manolivier93@gmail.com>
2024-03-03 10:53:54 +0200

    refactor the sidenav menu

---
+++ src/components/layouts/adminLayout/links.tsx
@@ -0,0 +1,196 @@
+import { TmenuItemLink } from 'types/links';
+import React from 'react';
+import {
+  DashboardOutlined,
+  UserOutlined,
+  BookOutlined,
+  BulbOutlined,
+  BarChartOutlined,
+  MessageOutlined,
+  HomeOutlined,
+  AppstoreAddOutlined,
+  BlockOutlined,
+  ContainerOutlined,
+} from '@ant-design/icons';
+import i18next from 'i18next';
+
+export const ADMIN_LINKS: TmenuItemLink = {
+  home: {
+    link: '/admin',
+    roles: ['admin'],
+    label: i18next.t('dashboard'),
+    key: 'admin_home',
+    icon: <DashboardOutlined />,
+    index: 0,
+  },
+  users: {
+    link: '/admin/users',
+    roles: ['admin'],
+    label: i18next.t('users'),
+    key: 'admin_users',
+    icon: <UserOutlined />,
+    index: 2,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/users/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/users/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  courses: {
+    link: '/admin/courses',
+    roles: ['admin'],
+    label: i18next.t('courses'),
+    key: 'admin_courses',
+    icon: <BookOutlined />,
+    index: 2,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/courses/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/courses/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  topics: {
+    link: '/admin/topics',
+    roles: ['admin'],
+    label: i18next.t('topics'),
+    key: 'admin_topics',
+    icon: <BulbOutlined />,
+    index: 3,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/topics/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/topics/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  schools: {
+    link: '/admin/schools',
+    roles: ['admin'],
+    label: i18next.t('schools'),
+    key: 'admin_schools',
+    icon: <HomeOutlined />,
+    index: 7,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/schools/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/schools/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  programs: {
+    link: '/admin/programs',
+    roles: ['admin'],
+    label: i18next.t('programs'),
+    key: 'admin_programs',
+    icon: <AppstoreAddOutlined />,
+    index: 8,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/programs/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/programs/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  units: {
+    link: '/admin/units',
+    roles: ['admin'],
+    label: i18next.t('units'),
+    key: 'admin_units',
+    icon: <BlockOutlined />,
+    index: 9,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/units/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/units/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  media_content: {
+    link: '/admin/media-content',
+    roles: ['admin'],
+    label: i18next.t('media-content'),
+    key: 'admin_media-content',
+    icon: <ContainerOutlined />,
+    index: 9,
+    children: [
+      {
+        label: 'create',
+        key: 'create',
+        link: '/admin/media-content/create',
+        roles: ['admin'],
+      },
+      {
+        label: 'edit',
+        key: 'edit',
+        link: '/admin/media-content/edit',
+        roles: ['admin'],
+      },
+    ],
+  },
+  statistics: {
+    link: '/statistics',
+    roles: ['admin'],
+    label: i18next.t('statistics'),
+    key: 'admin_statistics',
+    icon: <BarChartOutlined />,
+    index: 5,
+  },
+  feedback: {
+    link: '/feedback',
+    roles: ['admin'],
+    label: i18next.t('feedback'),
+    key: 'admin_feedback',
+    icon: <MessageOutlined />,
+    index: 6,
+  },
+};
---
+++ src/components/welcomePage/HeroSection.tsx
@@ -0,0 +1,98 @@
+import { Button, Flex } from 'antd';
+
+const HeroSection = () => {
+  return (
+    <>
+      <section className="bg-blue-50 dark:bg-gray-900">
+        <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+          <div className="place-self-center mr-auto lg:col-span-7">
+            <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none text-blue-900 md:text-5xl xl:text-6xl dark:text-white">
+              Your digital library
+            </h1>
+            <p className="mb-6 max-w-2xl font-light text-blue-700 lg:mb-8 md:text-lg lg:text-xl dark:text-blue-300">
+              Easily manage, your organization resources
+            </p>
+            <Button size={'large'} className="bg-blue-600 text-white">
+              Get Started
+            </Button>
+          </div>
+        </div>
+      </section>
+
+      <section className="bg-blue-100 dark:bg-gray-800">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+          <div className="mb-8 max-w-screen-md lg:mb-16">
+            <h2 className="mb-4 text-4xl font-extrabold text-blue-900 dark:text-white">
+              Designed for schools and large organizations
+            </h2>
+            <p className="text-blue-700 sm:text-xl dark:text-blue-300">
+              SparkEd is designed for schools and organizations, SparkEd is designed for schools and organizations.
+            </p>
+          </div>
+          <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+            <div className="flex flex-col items-center text-center">
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-200 lg:h-12 lg:w-12 dark:bg-blue-700">
+                <svg
+                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold text-blue-900 dark:text-white">Course based</h3>
+              <p className="text-blue-700 dark:text-blue-300">Organize contents for colleges with different courses</p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-200 lg:h-12 lg:w-12 dark:bg-blue-700">
+                <svg
+                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold text-blue-900 dark:text-white">University based</h3>
+              <p className="text-blue-700 dark:text-blue-300">
+                Ability to organize contents following the University structure
+              </p>
+            </div>
+            <div className="flex flex-col items-center text-center">
+              <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-blue-200 lg:h-12 lg:w-12 dark:bg-blue-700">
+                <svg
+                  className="w-5 h-5 text-blue-600 lg:w-6 lg:h-6 dark:text-blue-300"
+                  fill="currentColor"
+                  viewBox="0 0 20 20"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
+                    clip-rule="evenodd"
+                  ></path>
+                </svg>
+              </div>
+              <h3 className="mb-2 text-xl font-bold text-blue-900 dark:text-white">High School based</h3>
+              <p className="text-blue-700 dark:text-blue-300">
+                Contents can also be arranged as High Schools to help small schools manage their contents
+              </p>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
