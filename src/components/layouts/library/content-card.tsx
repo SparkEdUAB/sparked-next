b039d3e7a56369d5109ba1001d1fc8313b166149
Brian Mukuka <brianmuks@gmail.com>
2024-03-06 23:15:57 +0200

    apply blue background, unify the card with the theme

--- src/components/layouts/library/content-card.tsx
+++ src/components/layouts/library/content-card.tsx
@@ -1,22 +1,17 @@
-"use client";
+'use client';
 
-import {
-  DislikeOutlined,
-  LikeOutlined,
-  ShareAltOutlined,
-} from "@ant-design/icons";
-import { Avatar, Card, Typography } from "antd";
-import Image from "next/image";
-import React from "react";
+import { DislikeOutlined, LikeOutlined, ShareAltOutlined } from '@ant-design/icons';
+import { Avatar, Card, Typography } from 'antd';
+import Image from 'next/image';
+import React from 'react';
 
 const { Meta } = Card;
 
 const { Text, Link } = Typography;
 
-const ContentDetailsCardView: React.FC<{ title: string }> = ({
-  title = "",
-}) => (
+const ContentDetailsCardView: React.FC<{ title: string }> = ({ title = '' }) => (
   <Card
+    className="library-container"
     style={{ margin: 10, borderRadius: 20 }}
     cover={
       <Image
@@ -26,32 +21,31 @@ const ContentDetailsCardView: React.FC<{ title: string }> = ({
         src="https://cdn.pixabay.com/photo/2023/06/16/11/47/books-8067850_1280.jpg"
       />
     }
-    actions={[
-      <LikeOutlined key={1} />,
-      <DislikeOutlined key={2} />,
-      <ShareAltOutlined key="ellipsis" />,
-    ]}
+    actions={[<LikeOutlined key={1} />, <DislikeOutlined key={2} />, <ShareAltOutlined key="ellipsis" />]}
   >
     <Meta
-      avatar={
-        <Avatar src="https://cdn.pixabay.com/photo/2017/09/21/13/32/girl-2771936_1280.jpg" />
-      }
-      title={title}
-      description="This is the description"
+      className="content-details-card-meta-container"
+      avatar={<Avatar src="https://cdn.pixabay.com/photo/2017/09/21/13/32/girl-2771936_1280.jpg" />}
+      title={<span className="content-details-card-meta">{title}</span>}
+      description={<span className="content-details-card-meta">{'This is the description'}</span>}
     />
 
     <div
       style={{
-        display: "flex",
-        justifyContent: "space-around",
-        width: "100%",
+        display: 'flex',
+        justifyContent: 'space-around',
+        width: '100%',
         margin: 5,
-        alignItems: "center",
-        flexDirection: "row",
+        alignItems: 'center',
+        flexDirection: 'row',
       }}
     >
-      <Text type="secondary">200 views</Text>
-      <Text type="secondary">3hrs ago</Text>
+      <Text type="secondary">
+        <span className="content-details-card-meta-subtitle">200 views</span>
+      </Text>
+      <Text type="secondary">
+        <span className="content-details-card-meta-subtitle">3hrs ago</span>
+      </Text>
     </div>
   </Card>
 );
--- src/components/layouts/library/content-tags.tsx
+++ src/components/layouts/library/content-tags.tsx
@@ -1,16 +1,16 @@
-import { Tag } from "antd";
-import React from "react";
-import { libraryTags } from "./tags";
+import { Tag } from 'antd';
+import React from 'react';
+import { libraryTags } from './tags';
 
 const ContentTags: React.FC = () => (
   <div
     className="content-tag-container"
     style={{
-      maxWidth: "100%",
-      overflow: "scroll",
-      display: "flex",
+      maxWidth: '100%',
+      overflow: 'scroll',
+      display: 'flex',
 
-      flexDirection: "row",
+      flexDirection: 'row',
       marginLeft: 50,
       marginRight: 50,
     }}
@@ -20,13 +20,15 @@ const ContentTags: React.FC = () => (
         key={index}
         style={{
           height: 40,
-          textAlign: "center",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          textAlign: 'center',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
         }}
       >
-        <a href="#">{i}</a>
+        <a className="content-tag-item" href="#">
+          {i}
+        </a>
       </Tag>
     ))}
   </div>
