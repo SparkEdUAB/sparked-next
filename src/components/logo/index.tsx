3c17cb8c2582a8c9cbc8b0b6b46e1aebb202c47a
Yewo Mhango <mhangoyewoh@gmail.com>
2024-03-08 16:34:41 +0200

    Added new logo

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,13 +1,7 @@
-import Image from "next/image";
+import Image from 'next/image';
 
 const AppLogo = () => (
-    <Image
-      width={300}
-      height={60}
-      src="/logo-blue.png"
-      alt="SparkEd Logo"
-      className="admin-logo"
-    />
+  <Image width={160} height={30} src="/alternate-logo.svg" alt="SparkEd Logo" className="admin-logo" />
 );
 
 export default AppLogo;
