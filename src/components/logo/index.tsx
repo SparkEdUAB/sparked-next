b96dcadec4cc93a6dcb9da538abd0445f2237536
Yewo Mhango <mhangoyewoh@gmail.com>
2024-03-14 11:36:14 +0200

    Updated landing page

--- src/components/logo/index.tsx
+++ src/components/logo/index.tsx
@@ -1,7 +1,13 @@
 import Image from 'next/image';
 
-const AppLogo = () => (
-  <Image width={160} height={30} src="/alternate-logo.svg" alt="SparkEd Logo" className="admin-logo" />
+const AppLogo = ({ width, height }: { width?: number; height?: number }) => (
+  <Image
+    width={width || 160}
+    height={height || 30}
+    src="/alternate-logo.svg"
+    alt="SparkEd Logo"
+    className="admin-logo"
+  />
 );
 
 export default AppLogo;
--- src/components/welcomePage/HeaderSection.tsx
+++ src/components/welcomePage/HeaderSection.tsx
@@ -5,24 +5,24 @@ import Link from 'next/link';
 
 const HeaderSection = () => {
   return (
-    <header className="bg-blue-50  dark:bg-gray-800">
-      <nav className="max-w-screen-xl px-4 mx-auto lg:px-6 py-2.5 ">
-        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+    <header className="bg-sky-50  dark:bg-gray-900">
+      <nav className="max-w-screen-lg px-6 mx-auto py-2.5 ">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-lg">
           <a href="" className="flex items-center">
             <AppLogo />
           </a>
           <div className="flex items-center lg:order-2">
             <Flex gap="small" wrap="wrap" align="center">
-              <Link href="/auth/login" className="text-blue-600 font-medium mr-2">
+              <Link href="/auth/login" className="text-sky-600 dark:text-sky-400 font-medium mr-2">
                 Log in
               </Link>
               <Link
                 href="/auth/signup"
-                className="text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                className="text-white bg-sky-600 hover:bg-sky-700 focus:ring-4 focus:ring-sky-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-sky-600 dark:hover:bg-sky-700 focus:outline-none dark:focus:ring-sky-800"
               >
                 Sign Up
               </Link>
-              <DarkThemeToggle />
+              <DarkThemeToggle color="" className="text-sky-600 dark:text-sky-400" />
             </Flex>
           </div>
         </div>
