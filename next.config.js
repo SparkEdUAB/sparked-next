79369d6a8a50cd6068ae8d9dce31c8ebc89c7f18
Yewo Mhango <mhangoyewoh@gmail.com>
2024-04-01 19:25:24 +0200

    Added the sparked-next AWS bucket to nextConfig

--- next.config.js
+++ next.config.js
@@ -2,16 +2,22 @@
 const nextConfig = {
   env: {},
   webpack: (config) => {
-    config.externals = [...config.externals, { realm: "realm" }]; // required to make realm
+    config.externals = [...config.externals, { realm: 'realm' }]; // required to make realm
     return config;
   },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "cdn.pixabay.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'sparked-next.s3.eu-north-1.amazonaws.com',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
