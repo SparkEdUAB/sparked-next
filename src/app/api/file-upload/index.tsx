ec1319f86d4b00a2bc1ad6dedc0865fd40483b21
Yewo Mhango <mhangoyewoh@gmail.com>
2024-04-17 11:30:18 +0200

    Fixed mistaken process code

--- src/app/api/file-upload/index.tsx
+++ src/app/api/file-upload/index.tsx
@@ -1,13 +1,13 @@
-import { Session } from "next-auth";
-import s3Upload from "./s3";
-import MEDIA_PROCESS_CODES from "./processCodes";
+import { Session } from 'next-auth';
+import s3Upload from './s3';
+import SPARKED_PROCESS_CODES from 'app/shared/processCodes';
 
 export default async function uploadFile_(req: Request, session?: Session) {
   try {
     const formData = await req.formData();
-    const file = formData.get("file") as Blob;
+    const file = formData.get('file') as Blob;
 
-    let ext = file?.type.split("/")[1];
+    let ext = file?.type.split('/')[1];
 
     let arrayBuffer = await file?.arrayBuffer();
 
@@ -15,13 +15,13 @@ export default async function uploadFile_(req: Request, session?: Session) {
 
     const url = await s3Upload({
       file: buffer,
-      fileName: file.name.substring(0, file.name.lastIndexOf(".")),
+      fileName: file.name.substring(0, file.name.lastIndexOf('.')),
       ext,
     });
 
     const response = {
       isError: false,
-      code: MEDIA_PROCESS_CODES.METHOD_NOT_FOUND,
+      code: SPARKED_PROCESS_CODES.METHOD_NOT_FOUND,
       url,
     };
 
@@ -31,12 +31,10 @@ export default async function uploadFile_(req: Request, session?: Session) {
   } catch (error) {
     const response = {
       isError: true,
-      code: MEDIA_PROCESS_CODES.METHOD_NOT_FOUND,
+      code: SPARKED_PROCESS_CODES.METHOD_NOT_FOUND,
     };
     return new Response(JSON.stringify(response), {
       status: 200,
     });
   }
 }
-
-
