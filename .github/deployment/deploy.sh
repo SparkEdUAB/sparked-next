355c3ad1b29d3ebfef5a0f59262084155964af33
Olivier JM Maniraho <manolivier93@gmail.com>
2024-04-21 17:56:28 +0200

    refactor scripts

--- .github/deployment/deploy.sh
+++ .github/deployment/deploy.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
+# This scripts runs on the server to deploy the application, this is saved here for
+#tracking and reference purposes If updated here make sure it reflects on the
+# server our deployment directory is ~/htdocs/sparked-next Move the current
+# sparked-next directory to sparked-next-old if it exists
 
-# This scripts runs on the server to deploy the application, this is saved here for tracking and reference purposes
-# If updated here make sure it reflects on the server
+echo $(pwd);
 
-cd htdocs/
-# Create the releases directory if it doesn't exist
-mkdir -p releases;
-
-# Rename the current sparked-next directory to sparked-next-old
-if [ -d "sparked-next" ]; then
-  mv sparked-next-old sparked-next-older;
+if [ -d "htdocs/sparked-next" ]; then
+    mv htdocs/sparked-next htdocs/sparked-next-old || echo "Failed to move htdocs/sparked-next directory";
 fi
+
+cd htdocs
+mkdir -p sparked-next
+cd sparked-next
+
+# unarchize the file
+if [ -f "../archive.tar.gz" ]; then
+    tar -xvf ../archive.tar.gz
+else
+    echo "archive.tar.gz file does not exist";
+fi
+# Check if the sparked-next directory exists
+# Create the releases directory if it doesn't exist
+mkdir -p ../releases;
 
 # Copy .env from sparked-next-old to the new sparked-next
-if [ -d "sparked-next-old" ]; then
-  cp sparked-next-older/.env sparked-next/.env;
+if [ -f "../sparked-next-old/.env" ]; then
+    cp ../sparked-next-old/.env  ../sparked-next/.env;
 fi
-
-# Move into the new directory and run yarn
-cd sparked-next;
-yarn;
-yarn build;
-
-# Move sparked-next-old to the releases directory
-if [ -d "../sparked-next-old" ]; then
-  DATE=$(date +%Y%m%d%H%M);
-  mv ../sparked-next-old ../releases/sparked-next-old-$DATE;
-fi
+# Run yarn and build the project
 
 # Run the application
 forever start -c "yarn start" ./;
+
+# Move sparked-next-old to the releases directory
+DATE=$(date +%Y%m%d%H%M);
+mv "../sparked-next-old" "../releases/sparked-next-old-$DATE";
