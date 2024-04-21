074cd7cf1be90a7960422782c21859e592c6cda8
Olivier JM Maniraho <manolivier93@gmail.com>
2024-04-21 17:08:16 +0200

    use git clone instead of compressing

--- .github/deployment/deploy.sh
+++ .github/deployment/deploy.sh
@@ -9,16 +9,12 @@ mkdir -p releases;
 
 # Rename the current sparked-next directory to sparked-next-old
 if [ -d "sparked-next" ]; then
-  mv sparked-next sparked-next-old;
+  mv sparked-next-old sparked-next-older;
 fi
-
-# Extract the new archive.tar.gz into sparked-next
-mkdir sparked-next;
-tar -xzvf archive.tar.gz -C sparked-next;
 
 # Copy .env from sparked-next-old to the new sparked-next
 if [ -d "sparked-next-old" ]; then
-  cp sparked-next-old/.env sparked-next/.env;
+  cp sparked-next-older/.env sparked-next/.env;
 fi
 
 # Move into the new directory and run yarn
@@ -31,8 +27,6 @@ if [ -d "../sparked-next-old" ]; then
   DATE=$(date +%Y%m%d%H%M);
   mv ../sparked-next-old ../releases/sparked-next-old-$DATE;
 fi
-
-rm ../archive.tar.gz # Remove the archive.tar.gz file
 
 # Run the application
 forever start -c "yarn start" ./;
