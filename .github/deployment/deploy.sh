52cbc9a653f86cd08a91a83c4bf7c29dfb78ee47
Olivier JM Maniraho <manolivier93@gmail.com>
2024-04-21 16:20:38 +0200

    fix deployment scripts

---
+++ .github/deployment/deploy.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Create the releases directory if it doesn't exist
+mkdir -p releases;
+
+# Rename the current sparked-next directory to sparked-next-old
+if [ -d "sparked-next" ]; then
+  mv sparked-next sparked-next-old;
+fi
+
+# Extract the new archive.tar.gz into sparked-next
+mkdir sparked-next;
+tar -xzvf archive.tar.gz -C sparked-next;
+
+# Copy .env from sparked-next-old to the new sparked-next
+if [ -d "sparked-next-old" ]; then
+  cp sparked-next-old/.env sparked-next/.env;
+fi
+
+# Move into the new directory and run yarn
+cd sparked-next;
+yarn;
+
+# Move sparked-next-old to the releases directory
+if [ -d "../sparked-next-old" ]; then
+  DATE=$(date +%Y%m%d%H%M);
+  mv ../sparked-next-old ../releases/sparked-next-old-$DATE;
+fi
+
+rm ../archive.tar.gz # Remove the archive.tar.gz file
+
+# Run the application
+forever start -c "yarn start" ./;
