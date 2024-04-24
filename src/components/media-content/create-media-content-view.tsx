b979449666423cadfcfb63ec9b6ab3bcbc317c25
Yewo Mhango <mhangoyewoh@gmail.com>
2024-04-24 14:45:58 +0200

    Made thumbnail optional

--- src/components/media-content/create-media-content-view.tsx
+++ src/components/media-content/create-media-content-view.tsx
@@ -63,7 +63,7 @@ const CreateMediaContentView = ({ onSuccessfullyDone }: { onSuccessfullyDone?: (
 
     let topic = topics.find((topic) => topic._id === result.topicId);
 
-    if (!file || !thumbnail) {
+    if (!file) {
       return message.error(i18next.t('no_file'));
     }
 
@@ -76,11 +76,7 @@ const CreateMediaContentView = ({ onSuccessfullyDone }: { onSuccessfullyDone?: (
         return message.error(i18next.t('failed_to_upload'));
       }
 
-      let thumbnailUrl = await uploadFile(thumbnail);
-      if (!thumbnailUrl) {
-        setUploadingFile(false);
-        return message.error(i18next.t('failed_to_upload'));
-      }
+      let thumbnailUrl = thumbnail ? await uploadFile(thumbnail) : undefined;
 
       createResource(
         {
@@ -91,7 +87,7 @@ const CreateMediaContentView = ({ onSuccessfullyDone }: { onSuccessfullyDone?: (
           unitId: topic?.unitId,
         },
         fileUrl,
-        thumbnailUrl,
+        thumbnailUrl || undefined,
         onSuccessfullyDone,
       );
     } finally {
