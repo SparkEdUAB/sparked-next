7dcff60aa1ffb2a86b2b3245710b3061e3b82cec
Yewo Mhango <mhangoyewoh@gmail.com>
2024-04-29 20:58:49 +0200

    Removed unnecessary buttons

--- src/components/library/MediaContentView.tsx
+++ src/components/library/MediaContentView.tsx
@@ -26,20 +26,10 @@ export function MediaContentView({ mediaContent }: { mediaContent: T_RawMediaCon
           <PdfViewer file={mediaContent.file_url} />
         ) : null}
       </div>
-      {/* <a
-            className="mt-6 group inline-flex items-center justify-center px-4 py-2 text-center font-medium relative focus:z-10 focus:outline-none text-white bg-cyan-700 border border-transparent enabled:hover:bg-cyan-800 focus:ring-cyan-300 dark:bg-cyan-600 dark:enabled:hover:bg-cyan-700 dark:focus:ring-cyan-800 rounded-lg focus:ring-2"
-            href={mediaContent.fileUrl}
-            download
-            target="_blank"
-          >
-            <IoMdDownload className="mr-2 text-xl" /> Download
-          </a> */}
       <div>
         <h1 className="my-6 font-bold text-3xl">{mediaContent.name}</h1>
         <p className="text-lg whitespace-pre-wrap">{mediaContent.description}</p>
         <div className="my-6 flex flex-row flex-wrap text-gray-500 gap-x-8 gap-y-2">
-          {/* <IconWithLabel icon={<FaSchool className="text-2xl" />} label={mediaContent.schoolName} />
-          <IconWithLabel icon={<IoMdSchool className="text-2xl" />} label={mediaContent.programName} /> */}
           {mediaContent.course ? (
             <Link href={`/library?course_id=${mediaContent.course._id}`}>
               <IconWithLabel title="Course" icon={<ImBooks className="text-2xl" />} label={mediaContent.course.name} />
@@ -60,6 +50,7 @@ export function MediaContentView({ mediaContent }: { mediaContent: T_RawMediaCon
     </div>
   );
 }
+
 function IconWithLabel({ icon, label, title }: { label?: string; icon: ReactNode; title: string }) {
   return label ? (
     <div className="flex flex-row items-center gap-2" title={title}>
