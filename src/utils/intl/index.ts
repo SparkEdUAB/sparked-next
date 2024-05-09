7aabe5633df2a7ce94dd0a7714b0f173fbe866b7
Yewo Mhango <mhangoyewoh@gmail.com>
2024-05-09 16:31:18 +0200

    Extracted debug value for i18next to environment variable

--- src/utils/intl/index.ts
+++ src/utils/intl/index.ts
@@ -1,8 +1,9 @@
+import { DEBUG } from 'app/shared/constants';
 import i18next from 'i18next';
 
 i18next.init({
   lng: 'en', // if you're using a language detector, do not define the lng option
-  debug: true,
+  debug: DEBUG,
   saveMissing: true,
   resources: {
     en: {
