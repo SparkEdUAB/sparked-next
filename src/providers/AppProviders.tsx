06b8d90cb5a2b27cfa4d41d10bd16f7eb3dc0eb7
Olivier JM Maniraho <manolivier93@gmail.com>
2024-05-16 18:08:21 +0200

    setup error and usage tracking

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -5,6 +5,9 @@ import { Flowbite } from 'flowbite-react';
 import { ReactNode } from 'react';
 import { Session } from 'next-auth';
 import { AppProgressBar } from 'next-nprogress-bar';
+import { OpenpanelProvider } from '@openpanel/nextjs';
+import { HighlightInit } from '@highlight-run/next/client';
+import { NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, NEXT_PUBLIC_OPENPANEL_CLIENT_ID } from 'utils/constants';
 
 export default function AppProviders({
   children,
@@ -16,6 +19,24 @@ export default function AppProviders({
   return (
     <Flowbite>
       <SessionProvider session={session}>
+        <OpenpanelProvider
+          clientId={NEXT_PUBLIC_OPENPANEL_CLIENT_ID}
+          trackScreenViews={true}
+          trackAttributes={true}
+          trackOutgoingLinks={true}
+          profileId={session?.user?.email || 'anonymous'}
+        />
+        <HighlightInit
+          projectId={NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID}
+          serviceName="sparked-next"
+          tracingOrigins
+          excludedHostnames={['localhost', 'onrender.com']}
+          networkRecording={{
+            enabled: true,
+            recordHeadersAndBody: true,
+            urlBlocklist: [],
+          }}
+        />
         {children}
         <AppProgressBar color="#3584e4" height="4px" options={{ showSpinner: false }} />
       </SessionProvider>
