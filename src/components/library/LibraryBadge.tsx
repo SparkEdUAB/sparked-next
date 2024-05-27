27de717d41d318a8f97a6cc941640949bd28bdbd
Mike Njovu <mikemnjovu@gmail.com>
2024-05-27 15:45:47 +0200

    fix: library badges

--- src/components/library/LibraryBadge.tsx
+++ src/components/library/LibraryBadge.tsx
@@ -13,14 +13,14 @@ export default function LibraryBadge({
   return (
     <Link className="group" href={href}>
       <span
-        className={`flex text-center items-center gap-1 font-semibold ${
+        className={`flex text-center items-center gap-1 font-semibold  ${
           color === 'gray'
             ? 'bg-gray-100 text-gray-800 dark:bg-gray-700 dark:text-gray-300 group-hover:bg-gray-200 dark:group-hover:bg-gray-600'
             : 'bg-cyan-100 text-cyan-800 dark:bg-cyan-200 dark:text-cyan-800 group-hover:bg-cyan-200 dark:group-hover:bg-cyan-300'
-        } p-1 text-xs rounded px-2 py-0.5 h-full`}
+        } p-1 text-xs rounded px-3 py-2 h-full text-nowrap`}
         data-testid="flowbite-badge"
       >
-        <span>{children}</span>
+        {children}
       </span>
     </Link>
   );
