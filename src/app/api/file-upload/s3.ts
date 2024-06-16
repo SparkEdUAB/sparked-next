145776a3841adb0016614d8771cd3badab506403
Olivier JM Maniraho <manolivier93@gmail.com>
2024-06-16 12:49:39 +0200

    fix uploading with newer sdk

--- src/app/api/file-upload/s3.ts
+++ src/app/api/file-upload/s3.ts
@@ -1,4 +1,4 @@
-import { S3 } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, ObjectCannedACL } from '@aws-sdk/client-s3';
 
 type T_s3Upload = {
   file: Buffer;
@@ -10,34 +10,33 @@ const credentials = {
   accessKeyId: process.env.AWS_ACCESS_KEY_LOCAL as string,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
 };
-const s3Upload = ({ file, fileName, ext }: T_s3Upload) =>
-  new Promise((resolve, reject) => {
-    let imageUrl = getKeyName({ name: fileName, ext });
 
-    var objectParams: any = {
+const s3Client = new S3Client({
+  region: process.env.S3_REGION,
+  credentials,
+});
+
+async function uploadFile({ file, fileName, ext }: T_s3Upload) {
+  try {
+    const imageUrl = getKeyName({ name: fileName, ext });
+    const uploadParams = {
       Bucket: process.env.S3_BUCKET,
       Key: imageUrl,
       Body: file,
-      ACL: 'public-read',
+      ACL: 'public-read' as ObjectCannedACL,
     };
 
-    var uploadPromise = new S3({
-      credentials,
-    }).putObject(objectParams);
-    uploadPromise
-      .then(function () {
-        imageUrl = `${process.env.S3_BUCKET_NAME_URL}/${imageUrl}`;
-
-        resolve(imageUrl);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+    const uploadCommand = new PutObjectCommand(uploadParams);
+    await s3Client.send(uploadCommand);
+    return `${process.env.S3_BUCKET_NAME_URL}/${imageUrl}`;
+  } catch (error) {
+    throw error;
+  }
+}
 
 const today = new Date().getTime();
 
 const getKeyName = ({ name, ext }: { name: string; ext: string }) =>
   `${process.env.S3_MEDIA_CONTENT_FOLDER}/${name}-${today}.${ext}`;
 
-export default s3Upload;
+export default uploadFile;
