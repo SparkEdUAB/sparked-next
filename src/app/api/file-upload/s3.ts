8a02dc6518641ede7f147aa9317f6de88948f078
Olivier JM Maniraho <manolivier93@gmail.com>
2024-06-17 09:39:08 +0200

    199 upgrade aws sdk to v3 (#201)
    * upgrade aws sdk version to latest
    
    * fix uploading with newer sdk
    
    * update env sample

--- src/app/api/file-upload/s3.ts
+++ src/app/api/file-upload/s3.ts
@@ -1,4 +1,4 @@
-import { S3 } from 'aws-sdk';
+import { S3Client, PutObjectCommand, ObjectCannedACL } from '@aws-sdk/client-s3';
 
 type T_s3Upload = {
   file: Buffer;
@@ -6,38 +6,37 @@ type T_s3Upload = {
   ext: string;
 };
 
-const s3Upload = ({ file, fileName, ext }: T_s3Upload) =>
-  new Promise((resolve, reject) => {
-    let imageUrl = getKeyName({ name: fileName, ext });
+const credentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_LOCAL as string,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+};
 
-    var objectParams: any = {
+const s3Client = new S3Client({
+  region: process.env.S3_REGION,
+  credentials,
+});
+
+async function uploadFile({ file, fileName, ext }: T_s3Upload) {
+  try {
+    const imageUrl = getKeyName({ name: fileName, ext });
+    const uploadParams = {
       Bucket: process.env.S3_BUCKET,
       Key: imageUrl,
       Body: file,
-      ACL: 'public-read',
+      ACL: 'public-read' as ObjectCannedACL,
     };
 
-    var uploadPromise = new S3({
-      accessKeyId: process.env.AWS_ACCESS_KEY_LOCAL,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-      apiVersion: '2006-03-01',
-    })
-      .putObject(objectParams)
-      .promise();
-    uploadPromise
-      .then(function (_data) {
-        imageUrl = `${process.env.S3_BUCKET_NAME_URL}/${imageUrl}`;
-
-        resolve(imageUrl);
-      })
-      .catch((err) => {
-        reject(err);
-      });
-  });
+    const uploadCommand = new PutObjectCommand(uploadParams);
+    await s3Client.send(uploadCommand);
+    return `${process.env.S3_BUCKET_NAME_URL}/${imageUrl}`;
+  } catch (error) {
+    throw error;
+  }
+}
 
 const today = new Date().getTime();
 
 const getKeyName = ({ name, ext }: { name: string; ext: string }) =>
   `${process.env.S3_MEDIA_CONTENT_FOLDER}/${name}-${today}.${ext}`;
 
-export default s3Upload;
+export default uploadFile;
