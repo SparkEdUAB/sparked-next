47b42f7bb350f6f1b0aa5c859ec5ad4dcb97812b
Mike Njovu <mikemnjovu@gmail.com>
2024-07-14 16:17:55 +0200

    fix: searchMedia expecting 3 argument

--- src/fetchers/library/searchMedia.tsx
+++ src/fetchers/library/searchMedia.tsx
@@ -5,9 +5,9 @@ import { API_LINKS } from 'app/links';
 import NETWORK_UTILS from 'utils/network';
 import { MEDIA_CONTENT_LIMIT } from '@components/library/constants';
 
-type SORT_BY = string  ;
+type SORT_BY = string;
 
-export async function searchMedia(skip: number, searchText: string, sort_by: SORT_BY) {
+export async function searchMedia(skip: number, searchText: string, sort_by: SORT_BY = '') {
   return await fetcher<{ mediaContent: T_RawMediaContentFields[] }>(
     (BASE_URL || '') +
       API_LINKS.FIND_MEDIA_CONTENT_BY_NAME +
