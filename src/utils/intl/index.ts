82d33fb6fc73a8f5782220d9160b033680227e20
Yewo Mhango <mhangoyewoh@gmail.com>
2024-08-06 09:15:57 +0200

    Updated i18next DEBUG environment variable to be more descriptive

--- src/utils/intl/index.ts
+++ src/utils/intl/index.ts
@@ -1,9 +1,9 @@
-import { DEBUG } from 'app/shared/constants';
+import { I18NEXT_DEBUG_MODE } from 'app/shared/constants';
 import i18next from 'i18next';
 
 i18next.init({
   lng: 'en', // if you're using a language detector, do not define the lng option
-  debug: DEBUG,
+  debug: I18NEXT_DEBUG_MODE,
   saveMissing: true,
   resources: {
     en: {
