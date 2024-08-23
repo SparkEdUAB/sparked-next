2a66a20e89b18328f71969580086e203b75c31a0
Yewo Mhango <78272760+YewoMhango@users.noreply.github.com>
2024-08-23 12:15:53 +0200

    Added fixed size for media content card image (#237)

--- src/components/layouts/library/content-card.tsx
+++ src/components/layouts/library/content-card.tsx
@@ -1,4 +1,5 @@
 import { Card } from 'flowbite-react';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { truncateText } from 'utils/helpers/truncateText';
@@ -15,7 +16,18 @@ const ContentDetailsCardView = ({
   description: string;
 }) => (
   <Link href={url} className="h-full">
-    <Card className="max-w-sm mx-2 my-1 dark:bg-gray-700 h-full" imgAlt={title} imgSrc={image}>
+    <Card
+      className="max-w-sm mx-2 my-1 dark:bg-gray-700 h-full"
+      renderImage={() => (
+        <Image
+          className="rounded-t-lg aspect-[5/4] object-cover object-top"
+          alt={title}
+          src={image}
+          width={500}
+          height={400}
+        />
+      )}
+    >
       <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{title}</h5>
       <p className="font-normal text-gray-700 dark:text-gray-400">{truncateText(description, 80)}</p>
     </Card>
