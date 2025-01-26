ef2c5f88aab1c6f29536daceaabb3686b52a02ac
olivierjm <manolivier93@gmail.com>
2025-01-26 14:53:53 +0200

    remove trackers

--- src/providers/AppProviders.tsx
+++ src/providers/AppProviders.tsx
@@ -1,47 +1,27 @@
 'use client';
 
-import 'utils/intl';
-import { SessionProvider } from 'next-auth/react';
 import { Flowbite } from 'flowbite-react';
-import { ReactNode } from 'react';
 import { Session } from 'next-auth';
+import { SessionProvider } from 'next-auth/react';
 import { AppProgressBar } from 'next-nprogress-bar';
-import { OpenpanelProvider } from '@openpanel/nextjs';
-import { HighlightInit } from '@highlight-run/next/client';
-import { NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, NEXT_PUBLIC_OPENPANEL_CLIENT_ID } from 'utils/constants';
+import { ReactNode } from 'react';
+import 'utils/intl';
 import { ToastMessageProvider } from './ToastMessageContext';
 
-export default function AppProviders({
-  children,
-  session,
-}: {
+interface AppProvidersProps {
   children: ReactNode | ReactNode[];
   session: Session | null | undefined;
-}) {
+}
+
+const AppProviders: React.FC<AppProvidersProps> = ({ children, session }) => {
   return (
     <Flowbite>
       <SessionProvider session={session}>
-        <OpenpanelProvider
-          clientId={NEXT_PUBLIC_OPENPANEL_CLIENT_ID}
-          profileId={session?.user?.email || 'anonymous'}
-          trackScreenViews
-          trackAttributes
-          trackOutgoingLinks
-        />
-        <HighlightInit
-          projectId={NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID}
-          serviceName="sparked-next"
-          tracingOrigins
-          excludedHostnames={['localhost', 'onrender.com']}
-          networkRecording={{
-            enabled: true,
-            recordHeadersAndBody: true,
-            urlBlocklist: [],
-          }}
-        />
         <ToastMessageProvider>{children}</ToastMessageProvider>
         <AppProgressBar color="#3584e4" height="4px" options={{ showSpinner: false }} />
       </SessionProvider>
     </Flowbite>
   );
-}
+};
+
+export default AppProviders;
