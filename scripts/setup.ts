d10a911185a160ab8875e12cdcdb93e93915390b
Olivier JM Maniraho <manolivier93@gmail.com>
2025-03-11 21:41:38 +0200

    remove unused files

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,9 +1,7 @@
 import { initializeDatabase } from '@app/api/lib/db/init';
 
 async function setup() {
-  console.log('Initializing database...');
   await initializeDatabase();
-  console.log('Database initialized');
   process.exit(0);
 }
 
