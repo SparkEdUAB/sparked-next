1840070a0a5152a568ce003ebfebe2619b264f62
Olivier JM Maniraho <manolivier93@gmail.com>
2025-03-23 15:25:09 +0200

    remove swcMinify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {},
-  swcMinify: false,
   webpack: (config, { isServer }) => {
     // Handle externals for server-side builds
     if (isServer) {
