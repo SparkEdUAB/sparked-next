ba6deadde9e7d224436cfe84b323f70ce49a07cb
Mike Njovu <mikemtnjovu@gmail.com>
2025-08-13 02:18:59 +0200

    Implement session handling and role-based redirection in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,9 @@ import { Metadata, ResolvingMetadata } from 'next';
 import React from 'react';
 import { T_WelcomePageProps } from '@components/welcomePage/types';
 import { getMetadataGenerator } from 'utils/helpers/getMetadataGenerator';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import authOptions from './api/auth/authOptions';
 
 export async function generateMetadata(props: T_WelcomePageProps, parent: ResolvingMetadata): Promise<Metadata> {
   const getMetadata = await getMetadataGenerator(parent);
@@ -10,7 +13,14 @@ export async function generateMetadata(props: T_WelcomePageProps, parent: Resolv
   return getMetadata('SparkEd', 'Your digital library - Easily manage your organization resources');
 }
 
-const Home = () => {
+const Home = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session?.user) {
+    const role = session.user.role?.name || 'student';
+    if (role === 'student') redirect('/library');
+    else redirect('/admin');
+  }
   return <WelcomePage />;
 };
 
